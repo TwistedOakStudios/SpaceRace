5c01a8df85e6c840a5549f091ae07b6dd5976d44
Allan Lavell <alavell@gmail.com>
2013-05-16 17:01:14 -0300

    Refactored Round.

--- SpaceRaceII/Assets/_Scripts/Levels/Round.cs
+++ SpaceRaceII/Assets/_Scripts/Levels/Round.cs
@@ -1,16 +1,14 @@
 using UnityEngine;
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
 public class Round : MonoBehaviour {
-	public int numPlayers = 2;
-	public GameObject carPrefab1;
-	public GameObject carPrefab2;
+    public List<GameObject> carPrefabs;
 	public bool ended;
 	private bool canRestart;
 	public List<Car> cars;
-	public List<Car> killedCars;
 
     public float startingVelocity;
 	
@@ -22,34 +20,19 @@ public class Round : MonoBehaviour {
 	}
 	
 	void InitializeCars() {
-		cars = new List<Car>();
-		killedCars = new List<Car>();
-		
-		for (int i = 0; i < cars.Count; i++) {
-			Car car = cars[i];
-			cars.Remove(car);
-			Destroy(car.gameObject);
-		}
-		
-		for (int i = 0; i < numPlayers; i++) {
-			GameObject car;
-			if (i == 0) car = Instantiate(carPrefab1, Vector3.zero, Quaternion.identity) as GameObject;
-			else car = Instantiate(carPrefab2, Vector3.zero, Quaternion.identity) as GameObject;
-			cars.Add(car.GetComponent<Car>());
-		}
+        cars = new List<Car>();
+
+        for (int i = 0; i < carPrefabs.Count; i++) {
+            GameObject car = Instantiate(carPrefabs[i], Vector3.zero, Quaternion.identity) as GameObject;
+            car.GetComponent<Car>().Init();
+            cars.Add(car.GetComponent<Car>());
+        }
 	}
 	
 	public void StartRound() {
-        for (int i = 0; i < killedCars.Count; i++) {
-            cars.Add(killedCars[i]);
-            killedCars[i].gameObject.SetActiveRecursively(true);
-        }
-        killedCars.Clear();
-
         for (int i = 0; i < cars.Count; i++) {
-            Car car = cars[i];
-            car.Init();
-            car.Deploy(Static.LevelData.LaneManager.numberOfLanes / 2 - i, startingVelocity);
+            
+            cars[i].Deploy(Static.LevelData.LaneManager.numberOfLanes / 2 - i, startingVelocity);
         }
 
         ended = false;
@@ -58,24 +41,26 @@ public class Round : MonoBehaviour {
 	}
 	
 	void CarKilled(Car car) {
-		cars.Remove(car);
-		killedCars.Add(car);
-		if (cars.Count <= 1) {
-			StartCoroutine(EndRound());
-		}
+        if ((from c in cars select car.isDead).Count() >= carPrefabs.Count - 1) {
+            EndRound();
+        }
 	}
+
+    void EndRound() {
+        ended = true;
+        Static.Events.OnRoundEnded(this);
+        StartCoroutine(WaitBeforeAllowingRestart());
+    }
 	
-	IEnumerator EndRound() {
-		ended = true;
-		Static.Events.OnRoundEnded(this);
-		yield return new WaitForSeconds(1.0f);	
-		canRestart = true;
+	IEnumerator WaitBeforeAllowingRestart() {
+		yield return new WaitForSeconds(1.0f);
+        canRestart = true;
 	}
 	
 	void Update() {
 		if (ended && canRestart) {
 			if (Input.GetKeyDown(KeyCode.Space) || Input.touchCount > 0) {
-				Static.LevelData.Round.StartRound();
+                StartRound();
 			}
 		}
 	}
